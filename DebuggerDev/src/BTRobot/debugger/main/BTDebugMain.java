addc9324f2d48b332c42f8a6134c3da940635179
Scott Fisk <ehstechdept.scott@gmail.com>
2013-09-11 15:03:46 -0500

    Deleted edu template package and created BTRobot.debugger.main package

--- DebuggerDev/src/BTRobot/debugger/main/BTDebugMain.java
+++ DebuggerDev/src/BTRobot/debugger/main/BTDebugMain.java
@@ -5,7 +5,7 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package edu.wpi.first.wpilibj.templates;
+package BTRobot.debugger.main;
 
 
 import edu.wpi.first.wpilibj.SimpleRobot;
